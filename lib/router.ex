db3f4f882d9de2dc29ccfe1424da6aa23b49897f
Stefan Novak <stefannovak96@gmail.com>
2023-10-12 14:44:04 +0100

    feat: CORS

--- lib/router.ex
+++ lib/router.ex
@@ -2,6 +2,7 @@ defmodule Contactapi.Router do
   use Plug.Router
 
   plug(Plug.Logger)
+  plug CORSPlug, origin: &allowed_origins/1
 
   plug(:match)
 
@@ -16,4 +17,8 @@ defmodule Contactapi.Router do
   post "contact", do: send_resp(conn, 200, Jason.encode!(%{message: "Hello World"}))
 
   match _, do: send_resp(conn, 404, "Not Found")
+
+  defp allowed_origins(origin) do
+    origin in ["http://localhost:3000", "https://www.stefanalexnovak.com"]
+  end
 end
--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule Contactapi.MixProject do
   defp deps do
     [
       {:plug_cowboy, "~> 2.0"},
-      {:jason, "~> 1.4"}
+      {:jason, "~> 1.4"},
+      {:cors_plug, "~> 3.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
